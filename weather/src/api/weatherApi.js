7bba15ecfe70f426c69dad45b4646ff5b5c0825c
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-05-29 10:39:04 +0700

    initialization

--- weather/src/api/weatherApi.js
+++ weather/src/api/weatherApi.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const apiUrl = 'https://api.openweathermap.org/data/2.5/';
 
 export const getWeatherByCity = async (city) => {
+    // const apiKey = 95ee4a834483c3e2d1eb284c154019bc
     const apiKey = process.env.WEATHER_API_KEY;
     const endpoint = `${apiUrl}forecast?q=${city}&appid=${apiKey}`;
     const response = await axios.get(endpoint);
